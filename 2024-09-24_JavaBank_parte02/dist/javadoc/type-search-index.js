3a67d4c8da7b6dbf7f778b8592a5f3f45c60381d
Salustiano Oliveira <saluorodrigues@gmail.com>
2024-09-24 20:34:25 -0300

    JavaBank parte 02
    - Herança
    - Reescrita de métodos
    - Polimorfismo

---
+++ 2024-09-24_JavaBank_parte02/dist/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"br.com.javabank.modelo","l":"Conta"},{"p":"br.com.javabank.modelo","l":"Corrente"},{"p":"br.com.javabank","l":"JavaBank"},{"p":"br.com.javabank","l":"JavaBankHeranca"},{"p":"br.com.javabank.modelo","l":"Poupanca"}];updateSearchResults();
---
+++ 2024-09-24_JavaBank_parte02/src/br/com/javabank/modelo/Conta.java
@@ -0,0 +1,82 @@
+
+package br.com.javabank.modelo;
+
+/**
+ * Classe que representa um conta no sistema JavaBank
+ * @author aluno
+ */
+public class Conta{
+    private String titular;
+    private String numero;
+    protected double saldo;
+    
+    public Conta(String titular, String numero, double saldo){
+        //com parametros
+        this.numero = numero;
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+    
+    public Conta(){
+        //padrão
+    }
+    /**
+     * A função sacar remove fundos de uma conta
+     * @param valor referente ao valor a ser sacado
+     * @return retorna true caso o saque seja bem sucedido
+     */
+    public boolean sacar(double valor){
+        if(this.saldo - valor >= 0 && valor > 0){
+            this.saldo = this.saldo - valor;
+            System.out.println("Valor sacado: " + valor);
+            return true;
+        }else{
+            System.out.println("Saldo insuficiente!");
+            return false;
+        }
+    }
+    
+    //Impletar o deposito: Valor deverá ser positivo, retorna TRUE caso seja bem sucedido
+    public boolean depositar(double valor){
+        if(valor > 0){
+            this.saldo = this.saldo + valor;
+            return true;
+        }else{
+            System.out.println("Valor inválido!");
+            return false;
+        }
+    }    
+    
+    //Transferir saldo de uma conta para outra
+    public boolean transferir(double valor, Conta favorecido){
+        if(sacar(valor) == true){
+            favorecido.depositar(valor);
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    //Função GET sempre terá retorno do mesmo tipo da variavel
+    public double getSaldo(){
+        return this.saldo;
+    }
+    
+    //Função SET sempre será void e recebrá um  parametro do mesmo tipo da variavel
+    public void setTitular(String titular){
+        this.titular = titular;
+    }
+    
+    public String getTitular(){
+        return this.titular;
+    }
+    
+    public String getNumero(){
+        return this.numero;
+    }    
+    //Na classe corrente, reescrever o método imprimeSaldo para exibir alem do saldo 
+    //atual o saldo + o limite da conta.
+    public void imprimeSaldo(){
+        System.out.println("Saldo: R$%.2f\n".formatted(saldo));
+    }
+}
